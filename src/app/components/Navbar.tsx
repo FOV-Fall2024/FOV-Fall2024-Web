e1d90d2c97ce810fb9de1ba453cbe965a5d46dab
ducanhnt22 <ducanhbon4@gmail.com>
2024-06-16 16:18:54 +0700

    Build: base project

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/options";
+
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
+  console.log(session?.user);
+  return <header className="w-full">#Navbar</header>;
+}
